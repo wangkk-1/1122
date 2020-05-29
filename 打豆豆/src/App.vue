f0a4d696b5f96b94fd73b0e927f70e63c71b3e65
王凯凯 <w1998kk@sina.com>
2020-05-29 22:11:05 +0800

    打豆豆完成

---
+++ 打豆豆/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="todo-container">
+    <div class="todo-wrap">
+      <Header :addTodo="addTodo"></Header>
+      <Main :todos="todos" :deleteOne="deleteOne" :updateOne="updateOne"></Main>
+      <Footer :todos="todos" :updateAll="updateAll" :deleteAll="deleteAll"></Footer>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import Header from "@/components/Header";
+import Main from "@/components/Main";
+import Footer from "@/components/Footer";
+export default {
+  components: {
+    Main,
+    Header,
+    Footer
+  },
+
+  data() {
+    return {
+      todos: [
+        { id: 1, content: "抽烟", isOver: false },
+        { id: 2, content: "喝酒", isOver: true },
+        { id: 3, content: "烫头", isOver: false }
+      ]
+    };
+  },
+  methods: {
+    addTodo(obj) {
+      this.todos.unshift(obj);
+    },
+    deleteOne(index) {
+      this.todos.splice(index, 1);
+    },
+    updateOne(index, val) {
+      this.todos[index].isOver = val;
+    },
+    updateAll(val) {
+      this.todos.forEach(item => (item.isOver = val));
+    },
+    deleteAll() {
+      this.todos = this.todos.filter(item => !item.isOver);
+    }
+  }
+};
+</script>
+
+<style scoped>
+/*app*/
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+</style>
---
+++ 打豆豆/src/components/Main.vue
@@ -0,0 +1,45 @@
+<template>
+  <ul class="todo-main">
+    <Item
+      v-for="(todo,index) in todos"
+      :key="todo.id"
+      :todo="todo"
+      :index="index"
+      :deleteOne="deleteOne"
+      :updateOne="updateOne"
+    ></Item>
+  </ul>
+</template>
+
+<script type="text/ecmascript-6">
+import Item from "@/components/Item";
+export default {
+  props: {
+    todos: Array,
+    deleteOne: Function,
+    updateOne: Function
+  },
+  components: {
+    Item
+  }
+};
+</script>
+
+<style scoped>
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
